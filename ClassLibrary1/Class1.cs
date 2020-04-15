240ac27768f66a9fd108c89625765f8fa10e31c1
luciano060999martinez <62975046+luciano060999martinez@users.noreply.github.com>
2020-04-15 00:38:14 -0400

    feat: se añadio una libreria

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ClassLibrary1 //Libreria que contiene la conexion y para la entrada a la base de datos
+{
+    public class Utilidades
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+            SqlConnection Con = new SqlConnection("SERVER=LAPTOP-RRPVHD2G\\SQLEXPRESS;DATABASE=BDusuario;Integrated security=True"); // asigna a una variable una cadena de conexion para sql
+            Con.Open(); //abre la base de datos
+
+            DataSet DS = new DataSet(); // Una variable dataset para ir cargando los resultados del comando
+            SqlDataAdapter DP = new SqlDataAdapter(cmd,Con); // adapta el comando con la base de datos
+
+            DP.Fill(DS); // 
+
+            Con.Close(); // cierra la base de datos
+
+            return DS; //retorna un dataset con los pedidos
+        }
+
+
+    }
+}
